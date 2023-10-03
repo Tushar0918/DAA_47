143ce526326bec7db4fafae59dc1fa5b84c945e1
Tushar0918 <tushargaikwad497@gmail.com>
2023-10-03 16:40:48 +0530

    made some changes in B2

--- B2.cpp
+++ B2.cpp
@@ -10,5 +10,24 @@ int main()
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    return 0;
+    int pro[10],bp=arr[0];
+   
+    for(int i=0;i<n;i++) {
+        for(int j=1;j<n;j++){
+            if(bp<arr[j]){
+                pro[i]=arr[j]-bp;
+            }
+            
+        }
+    }
+    int count=0;
+    for(int i=0;i<n;i++)
+    {
+        if(pro[0] < pro[i])
+        {
+            pro[0]=pro[i];
+        }
+        // count=i;
+    }
+    cout<<"\nMaximun profit gained is "<<pro[0]<<" at day "<< count+1 << " at selling price "<<arr[count];
 }
